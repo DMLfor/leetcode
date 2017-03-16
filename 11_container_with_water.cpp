5866595eb0e177fa232c57be548a7fa6dc1b6c1a
DMLfor <daimeiliang@gmail.com>
2017-03-16 11:52:14 +0800

    leetcode第11题代码

---
+++ 11_container_with_water.cpp
@@ -0,0 +1,28 @@
+//双指针法
+class Solution {
+
+public:
+
+    int maxArea(vector<int>& height) {
+
+        int ans = 0, l = 0, r = height.size()-1;
+
+        while(l<r)
+
+        {
+
+            ans = max(ans, min(height[l] , height[r]) *(r - l));
+
+            if(height[l] < height[r])
+
+                l++;
+
+            else r--;
+
+        }
+
+        return ans;
+
+    }
+
+};
