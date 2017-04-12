44915b014f7f513bed82fbbfebd79f6ea964ae08
DMLfor <daimeiliang@gmail.com>
2017-04-12 21:14:28 +0800

    create

---
+++ 53_maximum_subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0, ans = INT_MIN;
+        for(int i =  0; i<nums.size(); i++)
+        {
+            sum += nums[i];
+            ans = max(sum, ans);
+            if(sum<0)
+                sum = 0;
+        }
+        return ans;
+    }
+};
